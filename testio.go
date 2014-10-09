28b36797f55f9bc49c543480418c2100d0767033
Kyle <kyle@tyrfingr.is>
2014-10-09 09:35:47 -0700

    Initial import.

---
+++ testio.go
@@ -0,0 +1,155 @@
+// Package testio implements various io utility types. Included are
+// BrokenWriter, which fails after writing a certain number of bytes;
+// a BufCloser, which wraps a bytes.Buffer in a Close method; and a
+// LoggingBuffer that logs all reads and writes.
+package testio
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+// BrokenWriter implements an io.Writer that fails after a certain
+// number of bytes. This can be used to simulate a network connection
+// that breaks during write or a file on a filesystem that becomes
+// full, for example. A BrokenWriter doesn't actually store any data.
+type BrokenWriter struct {
+	current, limit int
+}
+
+// NewBrokenWriter creates a new BrokenWriter that can store only
+// limit bytes.
+func NewBrokenWriter(limit int) *BrokenWriter {
+	return &BrokenWriter{limit: limit}
+}
+
+// Write will write the byte slice to the BrokenWriter, failing if the
+// maximum number of bytes has been reached.
+func (w *BrokenWriter) Write(p []byte) (int, error) {
+	if (len(p) + w.current) <= w.limit {
+		w.current += len(p)
+	} else {
+		spill := (len(p) + w.current) - w.limit
+		w.current = w.limit
+		return len(p) - spill, errors.New("write failed")
+	}
+
+	return len(p), nil
+}
+
+// Extend increases the byte limit to allow more data to be written.
+func (w *BrokenWriter) Extend(n int) {
+	w.limit += n
+}
+
+// Reset clears the limit and bytes in the BrokenWriter. Extend needs
+// to be called to allow data to be written.
+func (w *BrokenWriter) Reset() {
+	w.limit = 0
+	w.current = 0
+}
+
+// BufCloser is a buffer wrapped with a Close method.
+type BufCloser struct {
+	buf *bytes.Buffer
+}
+
+// Write writes the data to the BufCloser.
+func (buf *BufCloser) Write(p []byte) (int, error) {
+	return buf.buf.Write(p)
+}
+
+// Read reads data from the BufCloser.
+func (buf *BufCloser) Read(p []byte) (int, error) {
+	return buf.buf.Read(p)
+}
+
+// Close is a stub function to satisfy the io.Closer interface.
+func (buf *BufCloser) Close() {}
+
+// Reset clears the internal buffer.
+func (buf *BufCloser) Reset() {
+	buf.buf.Reset()
+}
+
+// Bytes returns the contents of the buffer as a byte slice.
+func (buf *BufCloser) Bytes() []byte {
+	return buf.buf.Bytes()
+}
+
+// NewBufCloser creates and initializes a new BufCloser using buf as
+// its initial contents. It is intended to prepare a BufCloser to read
+// existing data. It can also be used to size the internal buffer for
+// writing. To do that, buf should have the desired capacity but a
+// length of zero.
+func NewBufCloser(buf []byte) *BufCloser {
+	bc := new(BufCloser)
+	bc.buf = bytes.NewBuffer(buf)
+	return bc
+}
+
+// NewBufCloserString creates and initializes a new Buffer using
+// string s as its initial contents. It is intended to prepare a
+// buffer to read an existing string.
+func NewBufCloserString(s string) *BufCloser {
+	buf := new(BufCloser)
+	buf.buf = bytes.NewBufferString(s)
+	return buf
+}
+
+// A LoggingBuffer is an io.ReadWriter that prints the hex value of
+// the data for all reads and writes.
+type LoggingBuffer struct {
+	rw   io.ReadWriter
+	w    io.Writer
+	name string
+}
+
+// NewLoggingBuffer creates a logging buffer from an existing
+// io.ReadWriter. By default, it will log to standard error.
+func NewLoggingBuffer(rw io.ReadWriter) *LoggingBuffer {
+	return &LoggingBuffer{
+		rw: rw,
+		w:  os.Stderr,
+	}
+}
+
+// LogTo sets the io.Writer that the buffer will write logs to.
+func (lb *LoggingBuffer) LogTo(w io.Writer) {
+	lb.w = w
+}
+
+// SetName gives a name to the logging buffer to help distinguish
+// output from this buffer.
+func (lb *LoggingBuffer) SetName(name string) {
+	lb.name = name
+}
+
+// Write writes the data to the logging buffer and writes the data to
+// the logging writer.
+func (lb *LoggingBuffer) Write(p []byte) (int, error) {
+	if lb.name != "" {
+		fmt.Fprintf(lb.w, "[%s] ", lb.name)
+	}
+
+	fmt.Fprintf(lb.w, "[WRITE] %x\n", p)
+	return lb.rw.Write(p)
+}
+
+// Read reads the data from the logging buffer and writes the data to
+// the logging writer.
+func (lb *LoggingBuffer) Read(p []byte) (int, error) {
+	n, err := lb.rw.Read(p)
+	if err != nil {
+		return n, err
+	}
+	if lb.name != "" {
+		fmt.Fprintf(lb.w, "[%s] ", lb.name)
+	}
+
+	fmt.Fprintf(lb.w, "[READ] %x\n", p)
+	return n, err
+}
---
+++ testio_test.go
@@ -0,0 +1,124 @@
+package testio
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestBrokerWriter(t *testing.T) {
+	buf := NewBrokenWriter(2)
+	data := []byte{1, 2}
+
+	n, err := buf.Write(data)
+	if err != nil {
+		t.Fatalf("%v", err)
+	} else if n != 2 {
+		t.Fatalf("expected write size of 2, have %d", n)
+	}
+
+	_, err = buf.Write(data)
+	if err == nil {
+		t.Fatal("expected a write failure")
+	}
+
+	buf.Reset()
+	_, err = buf.Write(data)
+	if err == nil {
+		t.Fatalf("expected a write failure after reset")
+	}
+
+	buf.Extend(2)
+	_, err = buf.Write(data)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+}
+
+func TestBufCloser(t *testing.T) {
+	var data = []byte{1, 2}
+	var read = make([]byte, 2)
+
+	buf := NewBufCloser(data)
+	_, err := buf.Read(read)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	_, err = buf.Write(data)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	buf.Close()
+	buf.Reset()
+
+	s := "hi"
+	buf = NewBufCloserString(s)
+
+	read = buf.Bytes()
+	if string(read) != s {
+		t.Fatalf("expected %s, have %s", s, read)
+	}
+}
+
+func TestLoggingBuffer(t *testing.T) {
+	src := &bytes.Buffer{}
+	data := []byte("AB")
+	lb := NewLoggingBuffer(src)
+	_, err := lb.Write(data)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	src.Reset()
+	lb.SetName("TEST")
+	out := &bytes.Buffer{}
+	lb.LogTo(out)
+
+	_, err = lb.Write(data)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	expected := "[TEST] [WRITE] 4142\n"
+	if string(out.Bytes()) != expected {
+		t.Fatalf("expected '%s', have '%s'", expected, string(out.Bytes()))
+	}
+
+	out.Reset()
+	src = bytes.NewBuffer(data)
+	read := make([]byte, 2)
+
+	_, err = lb.Read(read)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	expected = "[TEST] [READ] 4142\n"
+	if string(out.Bytes()) != expected {
+		t.Fatalf("expected '%s', have '%s'", expected, string(out.Bytes()))
+	}
+
+	out.Reset()
+	lb.SetName("")
+	lb.LogTo(os.Stderr)
+	lb.Write([]byte("AB"))
+
+	lb.LogTo(out)
+	_, err = lb.Read(read)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	expected = "[READ] 4142\n"
+	if string(out.Bytes()) != expected {
+		t.Fatalf("expected '%s', have '%s'", expected, string(out.Bytes()))
+	}
+
+	src.Reset()
+	_, err = lb.Read(read)
+	if err == nil {
+		t.Fatal("expected a read failure")
+	}
+}
